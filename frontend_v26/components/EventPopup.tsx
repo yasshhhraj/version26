d89970383ca59a07139f0c6e134282378a016720
yashraj <yashraj.jangir69@gmail.com>
2026-01-29 11:05:29 +0530

    fix: update event descriptions

--- frontend_v26/components/EventPopup.tsx
+++ frontend_v26/components/EventPopup.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {EventCardData} from "@/components/event-poster";
 import {createPortal} from 'react-dom';
 import {X, Calendar, MapPin, Info, Link, Mail, LucideIcon} from 'lucide-react';
